fe9866637a7c846c2adcefc69c0ae5e755944412
Gercek Karakus <gercekk@gmail.com>
2015-10-03 14:15:58 +0300

    update index page and use some style and script

---
+++ script.js
@@ -0,0 +1,17 @@
+(function() {
+   // your page initialization code here
+   // the DOM will be available here
+
+   function getRandomColor() {
+       var letters = '0123456789ABCDEF'.split('');
+       var color = '#';
+       for (var i = 0; i < 6; i++ ) {
+           color += letters[Math.floor(Math.random() * 16)];
+       }
+       return color;
+   }
+
+   setInterval(function(){
+     document.body.style.background = getRandomColor();
+   }, 2000);
+})();
